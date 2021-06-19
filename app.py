07eb2fa261b631ea1541851f288a02ca1c664d88
Thierry <thierry.martins.r.s@gmail.com>
2021-06-18 20:36:43 -0400

    files changes

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+from bbquote.lib import get_quote
+
+"# Melhor Site da Galaxia"
+""
+""
+
+quote = get_quote()  # assuming the function returns an author and a quote
+
+f"{quote}"
